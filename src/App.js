c152c19c43c5f5e27ef84242ac23e6838d2253e5
Armando Silveira <Armando.x50@gmail.com>
2018-08-09 12:02:58 -0400

    Progress has been achieved

--- src/App.js
+++ src/App.js
@@ -11,17 +11,69 @@ function formatTime(t) {
   return t;
 }
 
+const ONE_DAY = 1000*60*60*24;
+const ONE_HOUR = 1000*60*60;
+const ONE_MINUTE = 1000*60;
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentTime: new Date(),
+      lastUpTime: new Date(1533748684000),
+      isUp: false
+    }
+  }
+
+  componentDidMount() {
+    this.intervalID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+
+  tick() {
+    let date = new Date();
+    this.setState({
+      currentTime: new Date()
+    });
+  }
+
   render() {
+    const timeDiff = this.state.currentTime - this.state.lastUpTime;
+    let dayDiff = Math.floor(timeDiff / ONE_DAY % 1);
+    let hourDiff = Math.floor(timeDiff / ONE_HOUR % 24);
+    let minuteDiff = Math.floor(timeDiff / ONE_MINUTE % 60);
+    
+    document.body.style.backgroundColor = this.state.isUp ? 'darkgreen' : 'darkred';
+    console.log(timeDiff);
+    console.log(dayDiff);
+    console.log(hourDiff);
+    console.log(minuteDiff);
     return (
         <div className="App">
           <Paper className="App-paper" elevation={20}>
-            <Typography variant="title">
-              The IPG API has been down for
-            </Typography>
-            <Typography variant='display4'>
-                {formatTime(new Date().getHours())}:{formatTime(new Date().getMinutes())}
-            </Typography>
+            { this.state.isUp 
+              ? 
+              <Typography>
+                The IPG API is up!
+              </Typography>
+              : 
+                <div>
+                  <Typography variant="title">
+                    It's been
+                  </Typography>
+                  <Typography className="App-typography" variant='display4'>
+                    {dayDiff ? dayDiff + ':': ''}{hourDiff}:{minuteDiff}
+                  </Typography>
+                  <Typography className="App-typography" variant='title'>
+                    since the API has been up.
+                  </Typography>
+                </div>
+            }
           </Paper>
         </div>
     );
